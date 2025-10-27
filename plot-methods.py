2efeadc690e68234505eb9d8845b620767ea6718
ogcelio <joaocelioaraujosander@gmail.com>
2025-10-26 22:35:55 -0300

    generalizing

--- plot-methods.py
+++ plot-methods.py
@@ -8,8 +8,7 @@ with open(path_data, "r", encoding="utf8") as file:
     data: dict = json.load(file)
 
 
-def plot_euler():
-
+def plot_methods():
     for method_name, method in data.items():
         fig, ax = plt.subplots(figsize=(9, 8))
         for step_size, step in method.items():
@@ -21,8 +20,8 @@ def plot_euler():
         plt.grid(True)
         plt.legend(loc="best")
         plt.tight_layout()
-        plt.show()
+    plt.show()
 
 
 if __name__ == "__main__":
-    plot_euler()
+    plot_methods()
