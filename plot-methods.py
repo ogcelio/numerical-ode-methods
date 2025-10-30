0adc27d6528160600f882c1eaa1e7780f2bb0ae6
ogcelio <joaocelioaraujosander@gmail.com>
2025-10-30 18:29:16 -0300

    plotting one graphic at a time

--- plot-methods.py
+++ plot-methods.py
@@ -1,3 +1,4 @@
+from numpy import arange, exp
 import json
 
 import matplotlib.pyplot as plt
@@ -9,18 +10,25 @@ with open(path_data, "r", encoding="utf8") as file:
 
 
 def plot_methods():
+    x = arange(0, 5, 0.001)
+    y = (200 + 26800 * exp(-2.7 * x)) / 27
     for method_name, method in data.items():
-        fig, ax = plt.subplots(figsize=(9, 8))
         for step_size, step in method.items():
+            fig, ax = plt.subplots(figsize=(10, 5))
+            ax.plot(x, y, label="Analítica", color="red", zorder=2)
+            plt.grid(True)
+            plt.tight_layout()
+            plt.xlabel("Tempo (s)")
+            plt.ylabel("N (Núcleos)")
             N = step["N"]
             t = step["t"]
-            ax.plot(t, N, label=f"{method_name}: h = {step_size}")
+            ax.plot(t, N, label=f"{method_name}: h = {step_size}", zorder=0)
             ax.scatter(t, N, s=20)
 
-        plt.grid(True)
-        plt.legend(loc="best")
-        plt.tight_layout()
-    plt.show()
+            plt.legend(loc="best")
+            plt.savefig(
+                f"pdf_images/fig_{method_name}_{step_size}.pdf", bbox_inches="tight"
+            )
 
 
 if __name__ == "__main__":
